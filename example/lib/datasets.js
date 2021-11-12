3c1202923a3c6ea7d4a7638f857cc8c1b8ac827e
brugmanjoost <joost@brugmanholding.com>
2021-11-12 16:15:09 +0100

    Create datasets.js

---
+++ example/lib/datasets.js
@@ -0,0 +1,169 @@
+module.exports = {
+
+    set1a: {
+        "4573cb2a": {
+            id: 1,
+            name: 'Johnny',
+        },
+        "f0b91a30": {
+            id: 2,
+            name: 'Susan',
+        },
+    },
+    set2a: {
+        "5dff2eef": {
+            identifer: { primaryKey: 102 },
+            name: 'Susan Brooks',
+        },
+        "bf336645": {
+            identifer: { primaryKey: 103 },
+            name: 'Paola DeVoss',
+        },
+    },
+
+    set1b: [
+        {
+            id: 1,
+            name: 'Johnny',
+        },
+        {
+            id: 2,
+            name: 'Susan',
+        },
+    ],
+    set2b: [
+        {
+            identifer: { primaryKey: 102 },
+            name: 'Susan Brooks',
+        },
+        {
+            identifer: { primaryKey: 103 },
+            name: 'Paola DeVoss',
+        },
+    ],
+
+    set1c: [1, 2],
+    set2c: [2, 3],
+
+
+    arrObjs1: [
+        {
+            id: 1,
+            name: 'Johnny',
+        },
+        {
+            id: 2,
+            name: 'Susan',
+        },
+    ],
+    arrObjs2: [
+        {
+            id: 2,
+            name: 'Susan Brooks',
+        },
+        {
+            id: 3,
+            name: 'Paola DeVoss',
+        },
+    ],
+
+
+
+
+
+    /*
+
+    mapObjs1d: {
+        "1fdf8620": {
+            id: 3,
+            name: 'Paola',
+        },
+        "0e0723ed": {
+            id: 4,
+            name: 'Frank',
+        },
+        "4573cb2a": {
+            id: 5,
+            name: 'Kimberly Brooks',
+        },
+        "f0b91a30": {
+            id: 6,
+            name: 'Francis de Tourville',
+        },
+    },
+
+
+    arrObjs2b: [
+        {
+            primaryKey: 3,
+            name: 'Paola DeVoss',
+        },
+        {
+            primaryKey: 4,
+            name: 'Frank Stilton',
+        },
+        {
+            primaryKey: 5,
+            name: 'Kimberly Brooks',
+        },
+        {
+            primaryKey: 6,
+            name: 'Francis de Tourville',
+        },
+    ],
+    mapObjs2b: {
+        "key-3": {
+            primaryKey: 3,
+            name: 'Paola DeVoss',
+        },
+        "key-4": {
+            primaryKey: 4,
+            name: 'Frank Stilton',
+        },
+        "key-5": {
+            primaryKey: 5,
+            name: 'Kimberly Brooks',
+        },
+        "key-6": {
+            primaryKey: 6,
+            name: 'Francis de Tourville',
+        },
+    },
+    mapObjs2c: {
+        "key-103": {
+            id: 103,
+            name: 'Paola DeVoss',
+        },
+        "key-104": {
+            id: 104,
+            name: 'Frank Stilton',
+        },
+        "key-105": {
+            id: 105,
+            name: 'Kimberly Brooks',
+        },
+        "key-106": {
+            id: 106,
+            name: 'Francis de Tourville',
+        },
+    },
+    mapObjs2d: {
+        "bf336645-9593-4365-bb95-8c3c9d0a6436": {
+            id: 103,
+            name: 'Paola DeVoss',
+        },
+        "b6f9388c-76d3-4128-b602-55359d49d7f0": {
+            id: 104,
+            name: 'Frank Stilton',
+        },
+        "5dff2eef-68b6-458b-85a4-aa5ddfe6e678": {
+            id: 105,
+            name: 'Kimberly Brooks',
+        },
+        "1ab4d8c4-f87d-46fe-8a4b-16530b2f3d14": {
+            id: 106,
+            name: 'Francis de Tourville',
+        },
+    }
+    */
+}
